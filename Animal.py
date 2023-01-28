1d9ae45063094b127b3f892aa655ddef0d8b5697
Nibraska12 <ni.bra@mail.ru>
2023-01-28 14:46:30 +0200

    added animal.py

---
+++ Animal.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def speak(self):
+        pass
+
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+
+class Lion(Cat):
+    def speak(self):
+        return "Roar!"
+
+
+class GoldenRetriever(Dog):
+    def speak(self):
+        return "Woof woof!"
+
+
+dog = Dog("Fido")
+cat = Cat("Whiskers")
+lion = Lion("Simba")
+golden_retriever = GoldenRetriever("Max")
+
+print(dog.speak())
+print(cat.speak())
+print(lion.speak())
+print(golden_retriever.speak())
